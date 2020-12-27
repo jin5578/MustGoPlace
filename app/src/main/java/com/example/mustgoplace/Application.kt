8ffa84f4cab17c32aad4e7c066e2fac1283cfd00
현성 <jin55789@naver.com>
2020-12-27 16:06:36 +0900

    HomeFragment Init Design

--- app/src/main/java/com/example/mustgoplace/Application.kt
+++ app/src/main/java/com/example/mustgoplace/Application.kt
@@ -2,7 +2,18 @@ package com.example.mustgoplace
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 
 @HiltAndroidApp
-class Application : Application()
+class Application : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+}
--- app/src/main/java/com/example/mustgoplace/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/mustgoplace/ui/home/HomeViewModel.kt
@@ -1,7 +1,35 @@
 package com.example.mustgoplace.ui.home
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
-class HomeViewModel @ViewModelInject constructor(): ViewModel() {
+class HomeViewModel @ViewModelInject constructor() : ViewModel() {
+
+    private val currentTime = Calendar.getInstance().time
+
+    private val _year = MutableLiveData<String>().apply { value = getYear() }
+    val year: LiveData<String>
+        get() = _year
+
+    private val _month = MutableLiveData<String>().apply { value = getMonth() }
+    val month: LiveData<String>
+        get() = _month
+
+
+    private fun getYear(): String {
+        val yearFormat = SimpleDateFormat("yyyy", Locale.KOREA)
+
+        return yearFormat.format(currentTime).toString()
+    }
+
+    private fun getMonth(): String {
+        val monthFormat = SimpleDateFormat("MM", Locale.KOREA)
+
+        return monthFormat.format(currentTime)
+    }
+
 }
